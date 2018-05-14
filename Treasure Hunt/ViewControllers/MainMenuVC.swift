0b4dd7bf16d0d36a5006dd0fba2a45b6433337cc
George Kolecsanyi <gyuri.kol@gmail.com>
2018-05-14 23:02:52 +1000

    return carriage removal

--- Treasure Hunt/ViewControllers/MainMenuVC.swift
+++ Treasure Hunt/ViewControllers/MainMenuVC.swift
@@ -9,11 +9,10 @@
 import UIKit
 
 class MainMenuVC: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
     }
 
     override func didReceiveMemoryWarning() {
