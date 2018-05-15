8afae20479feb752f426500de47a1719c00c9654
George Kolecsanyi <gyuri.kol@gmail.com>
2018-05-15 16:27:31 +1000

    segue preparation from main menu added

--- Treasure Hunt/ViewControllers/MainMenuVC.swift
+++ Treasure Hunt/ViewControllers/MainMenuVC.swift
@@ -19,7 +19,23 @@ class MainMenuVC: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        
+        if segue.destination is FriendVC
+        {
+            // to friend view controller
+            print("segue to friendvc")
+        } else if segue.destination is TreasureResourceVC
+        {
+            // to treasure resource view controller
+        }
+    }
 
 }
 
