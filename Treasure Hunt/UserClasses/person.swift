86a5080b9ac087854e4901efd8bda990705bd72d
George Kolecsanyi <gyuri.kol@gmail.com>
2018-05-15 16:28:26 +1000

    changed person details from struct to class

--- Treasure Hunt/UserClasses/person.swift
+++ Treasure Hunt/UserClasses/person.swift
@@ -9,8 +9,27 @@
 import Foundation
 
 // person structure acts as identity of person (user/friend)
-struct PersonDetails {
+class PersonDetails {
     // Properties
     var uid : String                // user identity
     var username : String           // user name
+    var firstname: String           // user firstname
+    var surname : String            // user surname
+    var email : String              // user email
+    
+    init(
+        // Class Initialization Parameters
+        UserID: String,
+        UserName: String,
+        FirstName: String,
+        Surname: String,
+        eMail: String
+    ) {
+        // Class Configuration Initialization from Parameters
+        uid = UserID
+        username = UserName
+        firstname = FirstName
+        surname = Surname
+        email = eMail
+    }
 }
