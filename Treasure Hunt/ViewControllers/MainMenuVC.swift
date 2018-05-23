b0867c5224eb24b005675c5149b1826b61e0a5df
George Kolecsanyi <gyuri.kol@gmail.com>
2018-05-23 23:23:48 +1000

    main menu populates test data now

--- Treasure Hunt/ViewControllers/MainMenuVC.swift
+++ Treasure Hunt/ViewControllers/MainMenuVC.swift
@@ -17,6 +17,18 @@ class MainMenuVC: UIViewController {
         // reference app delegate
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         
+        if !(appDelegate.currentUser.friends.count > 0) {
+            // TESTING - Add Custom Default User for startup
+            appDelegate.currentUser.treasures.append( Treasure( identifier: "0", Content: "testing 1 2 3", Destination: "Melbourne" ) )
+            appDelegate.currentUser.friends.append( User(details: PersonDetails(UserID: "a", UserName: "zOren", FirstName: "Zachery", Surname: "Orenstein", eMail: "testa@test.com") ) )
+            appDelegate.currentUser.friends.append( User(details: PersonDetails(UserID: "b", UserName: "poopmaster", FirstName: "Sherill", Surname: "Elia", eMail: "testb@test.com") ) )
+            appDelegate.currentUser.friends.append( User(details: PersonDetails(UserID: "c", UserName: "leLOL", FirstName: "Hilario", Surname: "Legrand", eMail: "testc@test.com") ) )
+            
+            appDelegate.currentUser.friends[0].treasures.append( Treasure(identifier: "1", Content: "Perth", Destination: "Perth" ) )
+            appDelegate.currentUser.friends[1].treasures.append( Treasure(identifier: "2", Content: "Darwin", Destination: "Darwin" ) )
+            appDelegate.currentUser.friends[2].treasures.append( Treasure(identifier: "3", Content: "Brisbane", Destination: "Brisbane" ) )
+        }
+        
         // construct default treasure map annotation focus
         appDelegate.userAnnotationsFocus = []
         for friend in appDelegate.currentUser.friends {
